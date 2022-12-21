8d82f07c9660328f00aa31d255018934a867af2b
limorko <limorkohanim15@gmail.com>
2022-12-21 14:35:55 -0500

    includes RST and beginning of SUB

--- lc3tools/lc3.f
+++ lc3tools/lc3.f
@@ -694,7 +694,7 @@ generate_instruction (operands_t operands, const char* opstr)
 	    	/* Check or read immediate range (error in first pass
 		   prevents execution of second, so never fails). */
 	        (void)read_val (o3, &val, 5);
-		write_value (0x1020 | (r1 << 9) | (r2 << 6) | (val & 0x1F));
+		write_value (0x1020 | (r1 << 9) | (r2 << 6) | ((val * (-1)) & 0x1F));
 	    } else
 		write_value (0x1000 | (r1 << 9) | (r2 << 6) | r3);
 	    break;
