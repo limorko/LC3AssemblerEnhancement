7ffe6284c81ffff32e69ca2a6f0531902ed5f1f4
limorko <limorkohanim15@gmail.com>
2023-01-01 22:36:11 -0500

    updating everything

--- lc3tools/test3.asm
+++ lc3tools/test3.asm
@@ -1,9 +1,12 @@
 	   .ORIG	x3000
+	   LD       R0, NEGTEN
 	   LD		R1, FIVE
+	   LD  		R2, NEGTEN
+	   LD		R3, NEGONE
 	   LD		R4, NEGTWO
 	   LD 		R5, THREE
-	   LD		R7, ONE
 	   LD		R6, ZERO
+	   LD		R7, ONE
 	   ;MLT  	R1, R6, R1
 	   ;MLT 		R3, R4, R6
 	   ;MLT 		R0, R6, R6
@@ -11,9 +14,8 @@
 	   ;MLT		R4, R6, R4
 	   ;MLT 		R6, R6, R6
 	   ;MLT 			R4, R4, R6
-	   MLT          R5, R5, R6
-	   MLT          R1, R5, R1
-	   ;MLT 		R3, R2, #2
+	   ;MLT          R5, R5, R6
+	   ;MLT          R1, R0, R1
 
 	  ; R1 = 0, R6 = 0
 	  ; R3 = 0, R5 = 3, R6 = 0
@@ -22,6 +24,8 @@
 	  ; R4 = R6 = 0
 	  ; R6 = 0
 	  ; R4 = -2 R1 =0
+	   ;MLT 		R3, R4, #2
+	   MLT 		R3, R2, #-10 
 	   HALT
 ;
 FIVE	    .FILL	x0005
@@ -29,5 +33,7 @@ THREE	   	.FILL	x0003
 ONE 		.FILL 	x0001
 NEGTWO		.FILL   xFFFE
 ZERO		.FILL	x0000
+NEGONE		.FILL	xFFFF
+NEGTEN		.FILL	XFFF6
 ;
 	   .END
