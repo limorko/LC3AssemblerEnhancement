0b2178f2c8ffb65cd5cbc19757cf80c35680855f
limorko <limorkohanim15@gmail.com>
2023-01-02 21:08:48 -0500

    adding more test cases

---
+++ lc3tools/ABS_test1.asm
@@ -0,0 +1,21 @@
+.ORIG	x3000
+LD      R0, ZERO
+LD		R1, TWO
+LD  	R2, NEGTHREE
+LD      R3, ONE
+LD      R4, NEGTWO
+LD      R5, THREE
+
+ABS R0, R1   ;R1 = positive number 
+
+HALT
+;
+ZERO	    .FILL	x0000
+TWO	   	    .FILL	x0002
+NEGTHREE	.FILL   xFFFD
+ONE         .FILL	x0001
+NEGTWO      .FILL	xFFFE
+THREE       .FILL	x0003
+;
+.END
+
---
+++ lc3tools/OPP_test.asm
@@ -0,0 +1,22 @@
+.ORIG	x3000
+LD      R0, ZERO
+LD		R1, TWO
+LD  	R2, NEGTHREE
+LD      R3, ONE
+LD      R4, NEGTWO
+LD      R5, THREE
+
+OPP R0 ; R0 = 0
+
+
+HALT
+;
+ZERO	    .FILL	x0000
+TWO	   	    .FILL	x0002
+NEGTHREE	.FILL   xFFFD
+ONE         .FILL	x0001
+NEGTWO      .FILL	xFFFE
+THREE       .FILL	x0003
+;
+.END
+
